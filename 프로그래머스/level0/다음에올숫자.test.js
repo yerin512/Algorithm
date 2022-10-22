f6d43693f1fe3c22443430650befea000879057c
yerin512 <sosu0512@naver.com>
2022-10-22 22:02:08 +0900

    다음에올숫자

---
+++ 프로그래머스/level0/다음에올숫자.test.js
@@ -0,0 +1,25 @@
+/* 
+문제: 등차수열 혹은 등비수열 common이 매개변수로 주어질 때, 마지막 원소 다음으로 올 숫자
+조건: 2 < common의 길이 < 1,000
+등차수열 혹은 등비수열이 아닌 경우는 없습니다.
+공비가 0인 경우는 없습니다.
+
+
+*/
+
+function solution(common) {
+  // 등차수열인지 확인
+  const sameMinus = common[1] - common[0] === common[2] - common[1];
+  if (sameMinus) {
+    return common[common.length - 1] + common[1] - common[0];
+  } else {
+    return common[common.length - 1] * (common[1] / common[0]);
+  }
+}
+
+describe('문자열의 가운데 글자를 구하라', () => {
+  it('문자열을 인자로 주면 가운데 글자를 리턴함', () => {
+    expect(solution([1, 2, 3, 4])).toBe(5);
+    expect(solution([2, 4, 8])).toBe(16);
+  });
+});
